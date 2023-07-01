31ef129092410125630ae33a55e21abb136684d0
Viktor <vitya.abrosimov19@gmail.com>
2023-07-01 13:47:36 +0300

    семинар 7

---
+++ Семинар_7/task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+// Исходный двумерный массив
+        
+
+void GetNumberFromMatrix(string coords){
+
+    int[,] array = {
+        {1, 4, 7, 2},
+        {5, 9, 2, 3},
+        {8, 4, 2, 4}
+    };
+
+    string[] position = coords.Split(',');
+
+    int row = Convert.ToInt32(position[0].Trim());
+    int column = Convert.ToInt32(position[1].Trim());
+
+    if (row >= 0 && row < array.GetLength(0) && column >= 0 && column < array.GetLength(1))
+    {
+        int value = array[row, column];
+        Console.WriteLine("Значение элемента: " + value);
+    }
+    else
+    {
+        Console.WriteLine("Такого элемента нет.");
+    }
+}
+
+Console.Write("Введите позицию элемента в формате \"строка, столбец\": ");
+string coords = Console.ReadLine();
+
+GetNumberFromMatrix(coords);
---
+++ Семинар_7/task_3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] array = 
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},        
+    {8, 4, 2, 4}
+};
+
+double[] GetColumnAverage (){
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    double[] columnAverages = new double[columns];
+
+    for (int column = 0; column < columns; column++)
+    {
+        int sum = 0;
+
+        for (int row = 0; row < rows; row++)
+        {
+            sum += array[row, column];
+        }
+
+        columnAverages[column] = (double)sum / rows;
+    }
+
+    return columnAverages;
+}
+
+void PrintColumnAverage(double[] columnAverages){
+    int columns = array.GetLength(1);
+    
+    Console.WriteLine("Среднее арифметическое каждого столбца:");
+
+    for (int column = 0; column < columns; column++)
+    {
+        Console.WriteLine($"Столбец {column + 1}: {Math.Round(columnAverages[column], 2)}");
+    }
+}
+
+GetColumnAverage();
+PrintColumnAverage(GetColumnAverage());
